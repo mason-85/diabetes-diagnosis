3d8bc683317c7fe519f7da85d7a83b7209cd90f1
mason-85 <156556839+mason-85@users.noreply.github.com>
2024-04-02 04:18:32 -0400

    Add files via upload

---
+++ dp_api.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, jsonify
+import joblib
+
+app = Flask(__name__)
+
+# Load the model from the file
+model = joblib.load('voting_clf.pkl')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    # Get the data from the POST request.
+    data = request.get_json(force=True)
+
+    # Make prediction using the model loaded from disk as per the data.
+    prediction = model.predict([data['input']])
+
+    # Return the entire prediction
+    output = prediction.tolist()  # Convert numpy array to list
+
+    return jsonify(output)
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
+
